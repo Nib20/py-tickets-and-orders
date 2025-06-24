2f1840fe74e8f0bd5fb1689750869a5864df2019
Nadiia Bohdan <nadiia.bohdan@holywater.tech>
2025-06-24 11:46:07 +0300

    Some fixes

--- db/models.py
+++ db/models.py
@@ -102,7 +102,7 @@ class Ticket(models.Model):
         if self.row > self.movie_session.cinema_hall.rows or self.row < 1:
             raise ValidationError(
                 {
-                    "row": [f"row number must be in available range:"
+                    "row": [f"row number must be in available range: "
                             f"(1, rows): (1, "
                             f"{self.movie_session.cinema_hall.rows})"]
                 }
@@ -113,7 +113,7 @@ class Ticket(models.Model):
         ):
             raise ValidationError(
                 {
-                    "seat": [f"seat number must be in available range:"
+                    "seat": [f"seat number must be in available range: "
                              f"(1, seats_in_row): (1, "
                              f"{self.movie_session.cinema_hall.seats_in_row})"]
                 }
